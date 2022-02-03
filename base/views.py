2110d42e91df230ac2ef8a62544f5d67c8fb242e
regalk13 <jsalazarcaviedes30@gmail.com>
2022-02-03 17:59:56 -0500

    :wrench: Add Databases & Admin panel (Rooms, Messages & Topics)

--- base/views.py
+++ base/views.py
@@ -1,22 +1,21 @@
 from django.shortcuts import render
+from .models import Room
 
-rooms = [
-    {'id':1, 'name':"Let's learn python!"},
-    {'id':2, 'name':"Design with me."},
-    {'id':3, 'name':"Just testing rooms."},
-]
+
+#rooms = [
+#    {'id':1, 'name':"Let's learn python!"},
+#    {'id':2, 'name':"Design with me."},
+#    {'id':3, 'name':"Just testing rooms."},
+#]
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-
+    room = Room.objects.get(id=pk)
     context = {'room':room}
     return render(request, 'base/room.html',context)
 
