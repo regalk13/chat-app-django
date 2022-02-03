87059339c65ef6f018c272e29ce583bacba49e5e
regalk13 <jsalazarcaviedes30@gmail.com>
2022-02-03 18:27:29 -0500

    :wrench: Update htmls

--- base/views.py
+++ base/views.py
@@ -19,3 +19,6 @@ def room(request, pk):
     context = {'room':room}
     return render(request, 'base/room.html',context)
 
+def createRoom(request):
+    context = {}
+    return render(request, 'base/room_form.html', context)
