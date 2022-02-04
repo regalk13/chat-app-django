aa035e2217417143df303f58ece6bf8a437b7f9b
regalk13 <jsalazarcaviedes30@gmail.com>
2022-02-04 11:19:20 -0500

    :wrench: Add basic login and logout user using `Django Auth Methods`

--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -8,6 +11,34 @@ from .forms import RoomForm
 #    {'id':2, 'name':"Design with me."},
 #    {'id':3, 'name':"Just testing rooms."},
 #]
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        
+        except:
+            messages.error(request, 'User does not exists')
+            
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+        else:
+            messages.error(request, 'Password are not correct')
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
 
 
 def home(request):
