06cbabc7a37c128b0aaa0482a1337fc73704e360
regalk13 <jsalazarcaviedes30@gmail.com>
2022-02-05 14:50:23 -0500

    :wrench: Work on static files `(can Add html/css/js)` and fix bug with room create

--- base/forms.py
+++ base/forms.py
@@ -6,6 +6,6 @@ class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__' # Gives all fields of Room class
-        
+        exclude = ['host', 'participants']
 
 
